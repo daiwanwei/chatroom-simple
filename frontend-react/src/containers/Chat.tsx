c05df3377a62bb8e50a17ecaa7107bc4f196016c
daiwanwei <wadejet.work@gmail.com>
2022-02-10 10:27:11 +0800

    initial commit

--- frontend-react/src/containers/Chat.tsx
+++ frontend-react/src/containers/Chat.tsx
@@ -1,19 +1,20 @@
 import React, {useState} from "react";
 import ChatBox from "../components/ChatBox";
 import ChatHistory from "../components/ChatHistory";
-import {enterRoom,sendMsgToRoom,receiveMsgFromRoom} from "../utils/chatRoom";
+import {enterRoom,sendMsgToRoom,receiveMsgFromRoom} from "../libs/chatRoom";
 
 enterRoom()
 
 function Chat() {
     let [msg,setMsg]=useState("nono");
     let [msgList,setMsgList]=useState([{text:"ann"}]);
+    console.log("test first")
     const changeMsg=(e:React.ChangeEvent<HTMLInputElement>)=>{
         let typedValue=e.target.value;
         setMsg(typedValue);
         console.log(`change msg${msg}`)
     }
-
+    console.log("test second")
     const sendMsg = (e:React.FormEvent<HTMLInputElement>) => {
         e.preventDefault();
         sendMsgToRoom(msg);
